f50a71ae71bcf26e040e679739f04d915783f586
Kajae Langlet <kajaelangley@gmail.com>
2021-12-07 08:47:03 -0500

    Initial Commit

---
+++ Kj wants to sleep and hates coding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kj wants to sleep and hates coding
+//
+//  Created by Kajae Langley on 12/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
